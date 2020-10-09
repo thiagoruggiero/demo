2d4440979e5aea882bdb8510099e958d23cc50d3
Thiago Ruggiero <thiago.ruggiero@avenuecode.com>
2020-10-09 15:39:41 -0300

    teste de commmit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -17,8 +17,8 @@ public class DemoApplication {
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		String s = "";
 		return String.format("Hello %s!", name);
-		teste
 	}
 
 }
